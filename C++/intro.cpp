8091fa0e73db9229fabd86223a3bb5b003ea3d99
diljotrandhawaa <learning287@gmail.com>
2023-06-28 09:38:31 -0700

    using namespace to remove 'std::' and endl for next line simple stuff`

--- C++/intro.cpp
+++ C++/intro.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
     int num1 = 1;
     int num2 = 2;
     int temp = num1;
     num1 = num2;
     num2 = temp;
-    std::cout << num1;
+    cout << "num1 = " << num1 << endl << "num2 = " << num2;
     return 0;
 }
